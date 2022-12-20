07c400206365dfbb1d4cee8a38f5f568b1fc4a31
Ingmund Olsson <ingmund@proton.me>
2022-12-20 03:48:49 +0200

    change: build script improvement

--- debpkg/src/build.sh
+++ debpkg/src/build.sh
@@ -4,26 +4,10 @@ set -e
 
 export CGO_ENABLED=0
 
-BUILDDIR=$(mktemp -d)
-
-cp -f keydesk/systemd/keydesk@.service "${BUILDDIR}"
-cp -f keydesk/systemd/keydesk@.socket "${BUILDDIR}"
-
 go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
-
-export GOBIN=${BUILDDIR}
-
 go install github.com/vpngen/keydesk/cmd/keydesk@latest
 
-CWD=$(pwd)
-cd ${BUILDDIR}
+nfpm package --config "${SHARED_BASE}/nfpm.yaml" --target "${SHARED_BASE}/pkg" --packager deb
 
-nfpm package --config /data/nfpm.yaml --target ${BUILDDIR} --packager deb
-
-cd ${CWD}
-
-cp -f ${BUILDDIR}/*.deb /data/pkg
-chown ${USER_UID}:${USER_UID} /data/pkg/*.deb
-rm -f ${BUILDDIR}/*
-rmdir ${BUILDDIR}
+chown ${USER_UID}:${USER_UID} "${SHARED_BASE}/pkg/"*.deb
 

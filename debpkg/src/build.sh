2c8de50a4d3f1fba92edff1e1638aa632436ba51
Ingmund Olsson <ingmund@proton.me>
2023-01-16 23:26:43 +0200

    fix

--- debpkg/src/build.sh
+++ debpkg/src/build.sh
@@ -4,10 +4,13 @@ set -e
 
 export CGO_ENABLED=0
 
-go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
-go install github.com/vpngen/keydesk/cmd/keydesk@latest
+RELEASE=${BRANCH:-"latest"}
 
-nfpm package --config "${SHARED_BASE}/nfpm.yaml" --target "${SHARED_BASE}/pkg" --packager deb
+go install github.com/vpngen/keydesk/cmd/keydesk@${RELEASE}
+
+go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
+
+nfpm package --config "keydesk/debpkg/nfpm.yaml" --target "${SHARED_BASE}/pkg" --packager deb
 
 chown ${USER_UID}:${USER_UID} "${SHARED_BASE}/pkg/"*.deb
 

6a7f614e70f7777f264829f5822d9d185d0a054e
Ingmund Olsson <ingmund@proton.me>
2023-01-17 21:28:37 +0200

    change: refactoring upgrade algs

--- debpkg/src/postinstall.sh
+++ debpkg/src/postinstall.sh
@@ -16,57 +16,7 @@ upgrade() {
     	printf "\033[32m Reload the service unit from disk\033[0m\n"
         systemctl stop 'keydesk@*.socket' 'keydesk@*.service' ||:
     	systemctl daemon-reload ||:
-
-	DBUSER=$(cat /etc/keydesk/dbuser)
-	DBNAME=$(cat /etc/keydesk/dbname)
-
-        sql=$(cat <<EOF
-BEGIN;
-
-ALTER TABLE IF EXISTS :"schema_name".brigade
-	ADD COLUMN IF NOT EXISTS create_at timestamp without time zone NOT NULL DEFAULT NOW();
-UPDATE :"schema_name".brigade SET create_at=(SELECT os_counter_mtime FROM :"schema_name".quota LIMIT 1);
-
-ALTER TABLE IF EXISTS :"schema_name".quota
-	ADD COLUMN IF NOT EXISTS os_counter_rx uint8 NOT NULL DEFAULT 0;
-ALTER TABLE IF EXISTS :"schema_name".quota
-	ALTER COLUMN os_counter_rx DROP DEFAULT;
-ALTER TABLE IF EXISTS :"schema_name".quota
-	ADD COLUMN IF NOT EXISTS os_counter_tx uint8 NOT NULL DEFAULT 0;
-ALTER TABLE IF EXISTS :"schema_name".quota
-	ALTER COLUMN os_counter_tx DROP DEFAULT;
-ALTER TABLE IF EXISTS :"schema_name".quota
-	DROP COLUMN IF EXISTS os_counter_value;
-
-CREATE TABLE IF NOT EXISTS :"schema_name".keydesk (
-        single_row_flag         bool UNIQUE NOT NULL DEFAULT true,
-        last_visit              timestamp without time zone DEFAULT NULL,
-        CONSTRAINT              single_row_check CHECK (single_row_flag = true)
-);
-
-INSERT INTO :"schema_name".keydesk DEFAULT VALUES ON CONFLICT DO NOTHING;
-
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA :"schema_name" TO :"schema_name";
-
-COMMIT;
-EOF
-)
-
-        for schema in $(sudo -u ${DBUSER} psql -d ${DBNAME} -t -c 'SELECT schema_name FROM information_schema.schemata;'); do
-                if [ "${schema}" = "public" -o "${schema}" = "pg_catalog" -o "${schema}" = "pg_toast" -o "${schema}" = "information_schema" -o "${schema}" = "_v" -o "${schema}" = "meta" ]; then
-                        continue
-                fi
-
-                echo "found: ${schema}"
-
-                echo "${sql}" | sudo -u ${DBUSER} psql -d ${DBNAME} -v ON_ERROR_STOP=yes --set schema_name="${schema}"
-                rc=$?
-                if [ ${rc} -ne 0 ]; then
-                        exit 1
-                fi
-        done
-
-	systemctl start --all 'keydesk@*.socket' ||:
+	systemctl restart --all 'keydesk@*.socket' ||:
 }
 
 # Step 2, check if this is a clean install or an upgrade

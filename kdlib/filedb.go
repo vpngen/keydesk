be7fd3fc8d82bf62ed098ff565b770f227d46d7b
Ingmund Olsson <ingmund@proton.me>
2023-02-10 19:55:51 +0200

    save

---
+++ kdlib/filedb.go
@@ -0,0 +1,68 @@
+package kdlib
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/vpngen/keydesk/kdlib/lockedfile"
+)
+
+const (
+	fileDbsuffix = ".tmp"
+	fileDbperm   = 0640
+)
+
+// FileDb - file pair as Db.
+type FileDb struct {
+	name string
+	r    *lockedfile.File
+	w    *lockedfile.File
+}
+
+// Close - rename tmp to main and close all files.
+func (f *FileDb) Close() error {
+	err := f.w.Sync()
+	if err != nil {
+		return fmt.Errorf("sync: %w", err)
+	}
+
+	err = os.Rename(f.name+fileDbsuffix, f.name)
+	if err != nil {
+		return fmt.Errorf("rename: %w", err)
+	}
+
+	f.w.Close()
+	f.r.Close()
+
+	return nil
+}
+
+// Decoder - get json.Decoder (main file).
+func (f *FileDb) Decoder() *json.Decoder {
+	return json.NewDecoder(f.r)
+}
+
+// Encoder - get json.Encoder (tmp file).
+func (f *FileDb) Encoder() *json.Encoder {
+	return json.NewEncoder(f.w)
+}
+
+// OpenFileDb - open file pair to edit.
+func OpenFileDb(name string) (*FileDb, error) {
+	w, err := lockedfile.OpenFile(name+fileDbsuffix, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileDbperm)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	r, err := lockedfile.OpenFile(name, os.O_RDWR|os.O_CREATE, fileDbperm)
+	if err != nil {
+		return nil, fmt.Errorf("edit: %w", err)
+	}
+
+	return &FileDb{
+		name: name,
+		r:    r,
+		w:    w,
+	}, nil
+}

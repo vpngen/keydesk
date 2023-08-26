a624483762eea5bbe05cfc9d57c8b6f79fb1f3e1
Ingmund Olsson <ingmund@proton.me>
2023-08-26 14:57:56 +0300

    add: add legacy support

--- cmd/replacebrigadier/replace_brigadier.sh
+++ cmd/replacebrigadier/replace_brigadier.sh
@@ -51,14 +51,17 @@ ovc_configs=""
 while [ "$#" -gt 0 ]; do
     case "$1" in
         -id)
+                NEW_STYLE="yes"
                 brigade_id="$2"
                 shift 2
                 ;;
         -ch)
+                NEW_STYLE="yes"
                 chunked="-ch"
                 shift 1
                 ;;
         -j)
+                NEW_STYLE="yes"
                 json="-j"
                 shift 1
                 ;;
@@ -87,19 +90,39 @@ while [ "$#" -gt 0 ]; do
                 shift 2
                 ;;
         -wg)
+                NEW_STYLE="yes"
                 wg_configs="-wg $2"
                 shift 2
                 ;;
         -ipsec)
+                NEW_STYLE="yes"
                 ipsec_configs="-ipsec $2"
                 shift 2
                 ;;
         -ovc)
+                NEW_STYLE="yes"
                 ovc_configs="-ovc $2"
                 shift 2
                 ;;
         *)
-                printdef "Unknown option: $1"
+                if [ -n "$NEW_STYLE" ]; then
+                        printdef "Unknown option: $1"
+                fi
+
+                if [ -z "$1" ]; then 
+                        printdef "Brigade ID is required"
+                fi
+
+                brigade_id="$1"
+                chunked=${2}
+
+                if [ "xchunked" != "x${chunked}" ]; then
+                        chunked=""
+                else
+                        chunked="-ch"
+                fi
+
+                break
                 ;;
     esac
 done

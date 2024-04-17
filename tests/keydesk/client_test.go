9f0122f2bd5d7a8d0aa3c8feebece3a83345262c
anon <anon@local>
2024-04-17 15:45:06 -0300

    Dockerfile; tests prototype

---
+++ tests/keydesk/client_test.go
@@ -0,0 +1,48 @@
+package keydesk
+
+import (
+	"flag"
+	clientruntime "github.com/go-openapi/runtime/client"
+	"github.com/vpngen/keydesk/gen/client"
+	"github.com/vpngen/keydesk/gen/client/operations"
+	"os"
+	"testing"
+)
+
+var host string
+
+func TestMain(t *testing.M) {
+	flag.StringVar(&host, "host", "localhost:8000", "server address")
+	flag.Parse()
+	os.Exit(t.Run())
+}
+
+func TestClient(t *testing.T) {
+	c := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{
+		Host: host,
+	})
+
+	token, err := c.Operations.PostToken(operations.NewPostTokenParams())
+	if err != nil {
+		t.Fatal(err)
+	}
+	authInfo := clientruntime.BearerToken(*token.Payload.Token)
+
+	t.Run("create user", func(t *testing.T) {
+		user, err := c.Operations.PostUser(nil, authInfo)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log(*user.Payload.UserName)
+	})
+
+	t.Run("get user", func(t *testing.T) {
+		users, err := c.Operations.GetUser(nil, authInfo)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, user := range users.Payload {
+			t.Log(*user.UserID, *user.UserName)
+		}
+	})
+}

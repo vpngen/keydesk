0924f4b889968e464044e9001542c2b08303ac65
Ingmund Olsson <ingmund@proton.me>
2024-08-25 19:44:08 +0300

    fix outline

--- internal/vpn/outline/client_config.go
+++ internal/vpn/outline/client_config.go
@@ -6,11 +6,13 @@ import (
 	"strings"
 
 	"github.com/vpngen/keydesk/internal/vpn/ss"
+	"github.com/vpngen/keydesk/keydesk"
 )
 
 func NewFromSS(name string, cfg ss.Config) (string, error) {
-	return fmt.Sprintf("%s#%s",
+	return fmt.Sprintf("%s/?outline=1&prefix=%s#%s",
 		cfg.GetConnString(),
+		keydesk.OutlinePrefix,
 		strings.ReplaceAll(url.QueryEscape(name), "+", "%20"),
 	), nil
 }

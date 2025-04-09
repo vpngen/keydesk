34592ccca5ffc4035e91dba55f7f3e8b6acc7be2
Ingmund Olsson <ingmund@proton.me>
2025-04-09 10:18:37 +0300

    fix: fix outline prefix

--- internal/vpn/outline/client_config.go
+++ internal/vpn/outline/client_config.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	"github.com/vpngen/keydesk/internal/vpn/ss"
-	"github.com/vpngen/keydesk/keydesk"
 )
+
+const Prefix = "%16%03%01%00%C2%A8%01%01"
 
 func NewFromSS(name string, cfg ss.Config) (string, error) {
 	return fmt.Sprintf("%s/?outline=1&prefix=%s",
 		cfg.GetConnString(),
-		keydesk.OutlinePrefix,
+		Prefix,
 		// strings.ReplaceAll(url.QueryEscape(name), "+", "%20"), // remove from socket
 	), nil
 }
